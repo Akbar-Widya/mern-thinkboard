0029012fe7c7c53699b2d6b5cb5a6dda0b959b79
Akbar-Widya <akbarwidya@student.telkomuniversity.ac.id>
2025-11-02 20:21:31 +0700

    Optimizing File & Folder Part Final: Controller folder & src folder.

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController"
+
+const router = express.Router()
+
+// endpoints: http_method, url, function
+router.get("/", getAllNotes)
+router.post("/", createNote)
+router.put("/:id", updateNote)
+router.delete("/:id", deleteNote)
+
+export default router
