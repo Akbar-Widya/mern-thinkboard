1e04911db0c5260485b82c6d8c1cfe90a582dc3b
Akbar-Widya <akbarwidya@student.telkomuniversity.ac.id>
2025-11-02 20:14:21 +0700

    Optimizing File & Folder Part One: Routes Folder.

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,11 @@
 import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
 
 const app = express()
 
-// Request-Response Cycle: Client (Customer) => API (Waiter) => Server (Kitchen) and back.
-// Function: The API is the messenger that routes the request and delivers the processed response (data).
-// Security: The API prevents the client from directly accessing or damaging the secure backend (kitchen/database).
+app.use("/api/notes", notesRoutes)
 
-
-app.get("/api/notes", (req, res) => {
-   res.status(200).send("you got 5 notes")
-})
-
-app.post("/api/notes", (req, res) => {
-   res.status(201).json({ message: "Note created successfully!"})
-})
-
-app.put("/api/notes/:id", (req, res) => {
-   res.status(200).json({ message: "Note updated successfully!"})
-})
-
-app.delete("/api/notes/:id", (req, res) => {
-   res.status(201).json({ message: "Note deleted successfully!"})
-})
-
-// Activate server
+// start server
 app.listen(5001, () => {
    console.log("Server started on PORT: 5001")
 })
