6442fd1a602fc91b6d8157f9414d60596d8ea35c
Akbar-Widya <akbarwidya@student.telkomuniversity.ac.id>
2025-11-02 20:04:20 +0700

    Still introducing endpoint in backend development. Apply small fix.

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,11 @@ app.post("/api/notes", (req, res) => {
    res.status(201).json({ message: "Note created successfully!"})
 })
 
-app.put("/api/notes", (req, res) => {
+app.put("/api/notes/:id", (req, res) => {
    res.status(200).json({ message: "Note updated successfully!"})
 })
 
-app.delete("/api/notes", (req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
    res.status(201).json({ message: "Note deleted successfully!"})
 })
 
