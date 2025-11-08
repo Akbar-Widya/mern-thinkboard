e1176c89352a3d6854337c601546507cb1fc2980
Akbar-Widya <akbarwidya@student.telkomuniversity.ac.id>
2025-11-08 17:56:31 +0700

    Reconfigure the rate limiter

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const ratelimit = new Ratelimit({
    redis: Redis.fromEnv(),
-   limiter: Ratelimit.slidingWindow(10, "60 s")
+   limiter: Ratelimit.slidingWindow(100, "600 s")
 })
 
 export default ratelimit
